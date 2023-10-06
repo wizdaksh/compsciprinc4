a33c08248491162a9eb5960f2666d345731ebbd2
Daksh Khandelwal <129240122+wizdaksh@users.noreply.github.com>
2023-10-05 20:16:11 -0400

    Add files via upload

---
+++ WS List Basics.py
@@ -0,0 +1,13 @@
+phrase = "VitaminChewable" 
+
+a = phrase[ 2 : 7 : 1 ] 
+
+c = phrase[ 1 : len( phrase ) : 3 ] 
+
+x = len( phrase ) - 5 
+
+y = len( phrase ) - 2 
+
+yep = phrase[ x : y : 1 ] 
+
+print( a )
